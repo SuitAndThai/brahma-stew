de1d78e3aa22a48a2174aa75715bccb2963f3674
knightbk@rose-hulman.edu <knightbk@rose-hulman.edu>
2012-10-04 19:08:09 -0400

    had the wrong personal record

---
+++ PersonalRecordExtension/src/recordextension/PersonalRecordExtension.java
@@ -0,0 +1,51 @@
+package recordextension;
+
+import java.awt.BorderLayout;
+import java.util.Set;
+
+import javax.swing.JPanel;
+
+
+import plugin.Plugin;
+import plugin.IPlugin;
+
+public class PersonalRecordExtension extends Plugin {
+	public static final String PLUGIN_ID = "Personal Record";
+
+	JPanel panel;
+	
+	public PersonalRecordExtension() {
+		super(PLUGIN_ID);
+	}
+
+	@Override
+	public void layout(JPanel parentPanel) {
+		parentPanel.setLayout(new BorderLayout());
+		panel = new PersonalRecordPanel();
+		parentPanel.add(panel);
+	}
+
+	@Override
+	public void start() {
+		// Nothing to initialize
+	}
+
+	@Override
+	public void stop() {
+		// Nothing to finalize
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<String> getDependencies() {		
+		return null;
+	}
+
+}
